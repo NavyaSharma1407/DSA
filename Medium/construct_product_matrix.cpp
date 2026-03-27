b0f879824d790aa2dad81a52fa4cbc0b3a893e1b
Navya Sharma <navya11748@gmail.com>
2026-03-27 23:00:53 +0530

    Leetcode 2906 - Construct Product Matrix

---
+++ Medium/construct_product_matrix.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int mod = 12345;
+        vector<long long>arr;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr.push_back(grid[i][j]);
+            }
+        }
+        int size = arr.size();
+        vector<long long> prefix(size,1) , suffix(size,1);
+
+        for(int i=1;i<size;i++){
+            prefix[i] = (prefix[i-1] * arr[i-1]) % mod;
+        }
+        for(int i=size-2;i>=0;i--){
+            suffix[i] = (suffix[i+1] * arr[i+1]) % mod;
+        }
+
+        int k=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                grid[i][j] = (prefix[k] * suffix[k]) % mod;
+                k++;
+            }
+        }
+        return grid;
+    }
+};
